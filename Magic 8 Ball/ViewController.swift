65a7e2ba599c211a3e5eeb577455f3ef702dad33
Fernando Motta <fernandosantosmotta@gmail.com>
2022-07-15 09:13:58 -0300

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Fernando dos Santos Motta on 15/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
